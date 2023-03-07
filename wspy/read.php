a5be77e261a8e8db2afd0f189812225de0c48eeb
Dejan Dundjerski <dejan.dundjerski@gmail.com>
2023-03-07 09:54:54 +0100

    fixes to work with php

---
+++ wspy/read.php
@@ -0,0 +1,32 @@
+<?php
+
+function sendToUrl($url, $data)
+{
+	$curl = curl_init($url);
+	curl_setopt($curl, CURLOPT_URL, $url);
+	curl_setopt($curl, CURLOPT_POST, true);
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+	$headers = array(
+		"Accept: application/json",
+		"Content-Type: application/json",
+	);
+	curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+	curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+	$resp = curl_exec($curl);
+	curl_close($curl);
+	var_dump($resp);
+}
+
+$oldLine = "";
+while (FALSE !== ($line = fgets(STDIN)))
+{
+	if ($oldLine != $line)
+	{
+		sendToUrl('http://localhost/index.php', $line);
+		sendToUrl('http://ddwwwhost.cloudapp.net/index.php', $line);
+		$oldLine = $line;
+	}
+}
+
+?>
--- www/index.php
+++ www/index.php
@@ -2,6 +2,8 @@
 $conn = new mysqli("localhost","root",trim(file_get_contents("pwd")),"weather");
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+	$json = file_get_contents('php://input');
+	$_POST = json_decode($json, true);
         if ($_POST['model'] == 'Cotech-367959' && $_POST['id'] = 152)
 	{
 		$query = "INSERT INTO data (time, battery_ok, temperature_C, humidity, rain_mm, wind_dir_deg, wind_avg_m_s, wind_max_m_s) VALUES ('" . $_POST['time'] . "'," . $_POST['battery_ok'] . ", '" . $_POST['temperature_C'] . "'," . $_POST['humidity'] . ",'" . $_POST['rain_mm'] . "'," . $_POST['wind_dir_deg'] . ",'" . $_POST['wind_avg_m_s'] . "','" . $_POST['wind_max_m_s'] . "' );";
