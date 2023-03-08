8751be6db84e86c38e060a69a554c378b39f9204
dejandundjerski <dejan.dundjerski@gmail.com>
2023-03-08 22:42:05 +0100

    watchdog implementation

--- wspy/watchdog.php
+++ wspy/watchdog.php
@@ -1,5 +1,14 @@
 <?php
 
-// select now()-max(time) from data;
+$conn = new mysqli("localhost","root",trim(file_get_contents("pwd")),"weather");
+$res = $conn->query("SELECT now() - max(time) AS 'last_snapshot_in_sec' FROM data;");
+
+while ($row = $res->fetch_assoc())
+{
+    if ($row['last_snapshot_in_sec'] > 300)
+    {
+        exec('sudo reboot');
+    }
+}
 
 ?>
