2686cd2c2fbca39e8e602fc92030415feda6e8e6
dejandundjerski <dejan.dundjerski@gmail.com>
2023-03-22 18:31:36 +0100

    Update push.php

--- www/push.php
+++ www/push.php
@@ -3,7 +3,7 @@
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	$json = file_get_contents('php://input');
 	$_POST = json_decode($json, true);
-        if ($_POST['model'] == 'Cotech-367959' && $_POST['id'] = 152)
+        if ($_POST['model'] == 'Cotech-367959')
 	{
 		$query = "INSERT INTO data (time, battery_ok, temperature_C, humidity, rain_mm, wind_dir_deg, wind_avg_m_s, wind_max_m_s) VALUES ('" . $_POST['time'] . "'," . $_POST['battery_ok'] . ", '" . $_POST['temperature_C'] . "'," . $_POST['humidity'] . ",'" . $_POST['rain_mm'] . "'," . $_POST['wind_dir_deg'] . ",'" . $_POST['wind_avg_m_s'] . "','" . $_POST['wind_max_m_s'] . "' );";
 	}
