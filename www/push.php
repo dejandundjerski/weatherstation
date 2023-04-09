05ed7bd83e8c154fcc817117ee4dcce155661494
dejandundjerski <dejan.dundjerski@gmail.com>
2023-04-09 08:32:51 +0200

    Update push.php
    add pressure handling to push.php

--- www/push.php
+++ www/push.php
@@ -5,7 +5,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 	$_POST = json_decode($json, true);
         if ($_POST['model'] == 'Cotech-367959')
 	{
-		$query = "INSERT INTO data (time, battery_ok, temperature_C, humidity, rain_mm, wind_dir_deg, wind_avg_m_s, wind_max_m_s) VALUES ('" . $_POST['time'] . "'," . $_POST['battery_ok'] . ", '" . $_POST['temperature_C'] . "'," . $_POST['humidity'] . ",'" . $_POST['rain_mm'] . "'," . $_POST['wind_dir_deg'] . ",'" . $_POST['wind_avg_m_s'] . "','" . $_POST['wind_max_m_s'] . "' );";
+		if (!isset($_POST['pressure']))
+		{
+			$_POST['pressure'] = 0;
+		}
+			    
+		$query = "INSERT INTO data (time, battery_ok, temperature_C, humidity, rain_mm, wind_dir_deg, wind_avg_m_s, wind_max_m_s, pressure) VALUES ('" . $_POST['time'] . "'," . $_POST['battery_ok'] . ", '" . $_POST['temperature_C'] . "'," . $_POST['humidity'] . ",'" . $_POST['rain_mm'] . "'," . $_POST['wind_dir_deg'] . ",'" . $_POST['wind_avg_m_s'] . "','" . $_POST['wind_max_m_s'] . "','" . $_POST['pressure'] . "' );";
 	}
 	else
 	{
