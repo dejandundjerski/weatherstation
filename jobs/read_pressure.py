9f6dc11ea53a0f6ae8fba6951f44ee5dc541c43a
dejandundjerski <dejan.dundjerski@gmail.com>
2023-04-15 14:28:39 +0200

    Update read_pressure.py

--- jobs/read_pressure.py
+++ jobs/read_pressure.py
@@ -1,12 +1,8 @@
 #!/usr/bin/env python
 from bmp280 import BMP280
+from smbus2 import SMBus
 
-try:
-    from smbus2 import SMBus
-except ImportError:
-    from smbus import SMBus
-    
 bus = SMBus(1)
 bmp280 = BMP280(i2c_dev=bus)
 pressure = bmp280.get_pressure()
-print(pressure)
+print(round(pressure, 2))
