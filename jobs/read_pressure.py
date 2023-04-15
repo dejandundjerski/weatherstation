d2f1143c47e18d8f9f4c7aca1b9fa10f596ccafb
dejandundjerski <dejan.dundjerski@gmail.com>
2023-04-15 14:26:47 +0200

    Update read_pressure.py

--- jobs/read_pressure.py
+++ jobs/read_pressure.py
@@ -5,13 +5,8 @@ try:
     from smbus2 import SMBus
 except ImportError:
     from smbus import SMBus
-
-# Initialise the BMP280
+    
 bus = SMBus(1)
 bmp280 = BMP280(i2c_dev=bus)
-
-temperature = bmp280.get_temperature()
 pressure = bmp280.get_pressure()
-
-output = {'inner_temperature':temperature,'pressure':pressure}
-print(output)
+print(pressure)
