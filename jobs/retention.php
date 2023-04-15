129442fb1c93f152139b08f0b115aad020b4c3dc
dejandundjerski <dejan.dundjerski@gmail.com>
2023-04-15 14:33:49 +0200

    Update retention.php

--- jobs/retention.php
+++ jobs/retention.php
@@ -2,14 +2,14 @@
 
 $conn = new mysqli("localhost","root",trim(file_get_contents("pwd")),"weather");
 
-if ($conn->query("DELETE FROM data WHERE time < now() - INTERVAL 8 day;"))
+if ($conn->query("DELETE FROM data WHERE time < now() - INTERVAL 3 day;"))
 {
     printf("data records older than 8 days deleted successfully.");
 }
 
 if ($conn->errno)
 {
-    printf("Could not delete records from table data: %s.", $conn->errno);
+    printf("Could not delete records from table [data]: %s.", $conn->errno);
 }
 
 ?>
